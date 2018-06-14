7bd04599ddbe9507e26b3a3ea4e8440d36ca3164
Isaac Auslander <isaacauslander@Isaacs-MacBook-Air.local>
2018-06-14 16:26:51 -0400

    fixed error with iOS resources and assets copying on iOS side.   This thing is looking better and better, my friends.

--- config/copy.config.js
+++ config/copy.config.js
@@ -1,16 +1,16 @@
 
 module.exports = {
+  copyFonts: {
+    src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/**/*', '{{ROOT}}/node_modules/ionic-angular/fonts/**/*'],
+    dest: '{{WWW}}/assets/fonts'
+  },
   copyAssets: {
-    src: ['{{SRC}}/assets/**/*'],
+    src: ['{{SRC}}/assets/*'],
     dest: '{{WWW}}/assets'
   },
   copyIndexContent: {
     src: ['{{SRC}}/index.html', '{{SRC}}/manifest.json', '{{SRC}}/service-worker.js'],
     dest: '{{WWW}}'
-  },
-  copyFonts: {
-    src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/**/*', '{{ROOT}}/node_modules/ionic-angular/fonts/**/*'],
-    dest: '{{WWW}}/assets/fonts'
   },
   copyPolyfills: {
     src: ['{{ROOT}}/node_modules/ionic-angular/polyfills/polyfills.js'],
